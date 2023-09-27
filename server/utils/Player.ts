14915d990be4bf6cf69e2d4d5667e9e7f0d39bc5
Youki <ylewis@c-lockinc.com>
2023-09-27 21:47:23 +0900

    Created Player class for defining players for game

--- server/utils/Player.ts
+++ server/utils/Player.ts
@@ -1,7 +1,18 @@
 class Player {
   score: number;
-  id: string;
+  id: number;
   username: string;
+
+ constructor(username: string, id: number) {
+    this.score = 0;
+    this.username = username;
+    this.id = id;
+  }
+
+  updateScore(earnedScore: number) {
+    this.score += earnedScore;
+  }
+  
 }
 
 export default Player;
